429bf234f9346b6c451f1b21c1b0950eb622078d
Martin Petkov <martin.p.petkov@gmail.com>
2016-11-19 08:44:18 -0500

    Add extension files

---
+++ add-explain-link.js
@@ -0,0 +1,5 @@
+var regexp = /Permanent link.*http:\/\/xkcd.com\/([0-9]+).*/g;
+var xkcdNum = regexp.exec($('body').html())[1];
+var explainxkcdLink = "http://www.explainxkcd.com/wiki/index.php/" + xkcdNum;
+var explainxkcdTag = "<br><a href=" + explainxkcdLink + ">Link to explanation</a>";
+$("#transcript").before(explainxkcdTag);
